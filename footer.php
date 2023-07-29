fc5089ecfa629cef3e476035240534ab663d5f53
BiznesMeM <kosmosdrago@gmail.com>
2023-07-29 15:04:01 +0300

    The basement of the site

---
+++ footer.php
@@ -0,0 +1,81 @@
+</section>
+
+            <footer class="Partnership">
+
+                <div class="header">
+
+                    <div class="header__logo">
+            
+                        <div class="logo">
+            
+                            <img class="img_logo" src="<?php bloginfo('template_url'); ?>/assets/image-22x22//logo_22x22.png">
+            
+                        </div>
+            
+                        <div class="link">
+            
+                            <h3 class="title_link">franch-remont.ru</h3>
+            
+                        </div>
+            
+                    </div>
+            
+                </div>
+
+                <div class="Partnership__text">
+                               
+                    <h1 class="Partnership__text-title"><b>партнерство с нами</b>
+                        это системный бизнес в стабильной нише<img class="Partnership__text-image" src="<?php bloginfo('template_url'); ?>/assets/icon-22x22/icon__partnership.png"></h1>
+
+                </div>
+
+                <div class="Partnership-block-div">
+
+                    <div class="Partnership-block">
+
+                        <div class="Partnership-block-text">
+    
+                            <img class="Partnership-block-text-image" src="">
+    
+                            <p class="Partnership-block__text-paragraph">Пройти множество<br>
+                                курсов и набить <br>
+                                собственные шишки</p>
+    
+                        </div>
+    
+                    </div>
+    
+                    <div class="Partnership-block Partnership-block-yellow">
+    
+                        <div class="Partnership-block-text">
+    
+                            <img class="Partnership-block-text-image" src="">
+    
+                            <p class="Partnership-block__text-paragraph">Открыть франшизу <br>
+                                и выйти
+                                на <br>стабильный доход</p>
+    
+                        </div>
+    
+                    </div>
+
+                </div>
+
+                <div class="Partnership__bottom-image">
+
+                    <img class="Partnership__bottom-img" src="<?php bloginfo('template_url'); ?>/assets/image-22x22//philosofer__image-png.png">
+
+                </div>
+
+            </footer>
+
+            <div class="Create">
+
+                <p class="create__name">@Arkadiy K.</p>
+
+            </div>
+
+        </div>
+
+</body>
+</html>
