dfe146a4728d2e6626cfc4be1963fa23f421e23c
BiznesMeM <kosmosdrago@gmail.com>
2023-07-29 15:04:58 +0300

    The brain of the site

---
+++ header.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    
+    <meta <?php bloginfo('charset'); ?>>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>22x22</title>
+
+    <?php wp_head(); ?>
+    
+</head>
+<body>
+
+    <div class="container">
+
+        <header class="header">
+
+            <div class="header__logo">
+    
+                <div class="logo">
+    
+                    <img class="img_logo" src="<?php bloginfo('template_url'); ?>/assets/image-22x22//logo_22x22.png">
+    
+                </div>
+    
+                <div class="link">
+    
+                    <h3 class="title_link">franch-remont.ru</h3>
+    
+                </div>
+    
+            </div>
+    
+        </header>
+
+        <div class="Philosopher">
